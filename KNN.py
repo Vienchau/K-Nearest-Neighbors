8b01f174fd781d653283d766909f67a1b4d1ece3
VienChau <Chautrivien2103@gmail.com>
2022-01-17 16:54:41 +0700

    add plot

--- KNN.py
+++ KNN.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import LabelEncoder
 
@@ -32,7 +31,7 @@ class KNN:
 
 
 	def Data_Classifier(self, _X_train, _X_test, _y_train, _y_test, _n_neighbors):
-		classifier = KNeighborsClassifier(n_neighbors=_n_neighbors)
+		classifier = KNeighborsClassifier(n_neighbors=_n_neighbors, weights= 'distance')
 		classifier.fit(_X_train, _y_train)
 		predictions = classifier.predict(_X_test)
 		accu = (self.Accurate(_y_test, predictions) * 100)
@@ -42,21 +41,19 @@ class KNN:
 		sc = StandardScaler()
 		_X_train2 = sc.fit_transform(_X_train2)
 		_X_test2 = sc.transform(_X_test2)
-
-		classifier = KNeighborsClassifier(n_neighbors=_n_neighbors)
+		classifier = KNeighborsClassifier(n_neighbors=_n_neighbors,weights= 'distance')
 		classifier.fit(_X_train2, _y_train2)
 		predictions2 = classifier.predict(_X_test2)
 		accu = (self.Accurate(_y_test2, predictions2) * 100)
 		return accu 
 
 	def Data_Classifier_with_Nomalize(self, _X_train2, _X_test2, _y_train2, _y_test2, _n_neighbors):
-			scaler = MinMaxScaler(feature_range=(0,1))
-			_X_train2[:, :2] = scaler.fit_transform(_X_train2[:, :2])
-			_X_test2[:, :2] = scaler.transform(_X_test2[:, :2])
-	
-			classifier = KNeighborsClassifier(n_neighbors=_n_neighbors)
-			classifier.fit(_X_train2, _y_train2)
-			predictions2 = classifier.predict(_X_test2)
-			accu = (self.Accurate(_y_test2, predictions2) * 100)
-			return accu 
+		scaler = MinMaxScaler(feature_range=(0,1))
+		_X_train2[:, :2] = scaler.fit_transform(_X_train2[:, :2])
+		_X_test2[:, :2] = scaler.transform(_X_test2[:, :2])
+		classifier = KNeighborsClassifier(n_neighbors=_n_neighbors, weights= 'distance')
+		classifier.fit(_X_train2, _y_train2)
+		predictions2 = classifier.predict(_X_test2)
+		accu = (self.Accurate(_y_test2, predictions2) * 100)
+		return accu 
 
--- KNN_test.py
+++ KNN_test.py
@@ -6,20 +6,48 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import classification_report, confusion_matrix
+import seaborn as sns
 
 from KNN  import  KNN
 
 columns = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'Class']
-Iris_Data = data_df = pd.read_csv("./iris.data", names = columns)
+Iris_Data = pd.read_csv("iris.data", names = columns)
 KNN_test = KNN(Iris_Data)
 
-X_train, X_test, y_train, y_test = KNN_test.Data_splitXY()
+Raw = np.zeros((5,4))
 
-acc1 = KNN_test.Data_Classifier(X_train, X_test, y_train, y_test, 11)
+for i in range (5):
+    X_train, X_test, y_train, y_test = KNN_test.Data_splitXY()
+
+    acc1 = KNN_test.Data_Classifier(X_train, X_test, y_train, y_test, 9)
+    Raw[i,0] = i
+    Raw[i,1] = acc1
+    acc2 = KNN_test.Data_Classifier_with_Standard(X_train, X_test, y_train, y_test, 9)
+    Raw[i,2] = acc2
+    acc3 = KNN_test.Data_Classifier_with_Nomalize(X_train, X_test, y_train, y_test, 9)
+    Raw[i,3] = acc3
+
+
 print(acc1)
-
-acc2 = KNN_test.Data_Classifier_with_Standard(X_train, X_test, y_train, y_test, 11)
 print(acc2)
-
-acc3 = KNN_test.Data_Classifier_with_Nomalize(X_train, X_test, y_train, y_test, 11)
 print(acc3)
+
+plt.figure(figsize= (20,3), dpi= 100)
+plt.plot(Raw[:,0], Raw[:,1], label = 'Raw')
+plt.plot(Raw[:,0], Raw[:,2], label = 'Standardization')
+plt.plot(Raw[:,0], Raw[:,3], label = 'Normalization')
+
+
+plt.title('3 times test')
+plt.xlabel('test')
+plt.ylabel('Accuracy')
+plt.legend(loc ='best')
+plt.grid() 
+plt.show()
+
+
+
+
+
+
+
